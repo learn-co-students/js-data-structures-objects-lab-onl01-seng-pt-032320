15d935472aee7217b4062bcddbbe3b6ead195936
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-06 06:03:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, val) {
+  return Object.assign({}, obj, { [key]: val });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, val) {
+  return Object.assign(obj, { [key]: val });
+}
+
+function deleteFromDriverByKey(obj, key) {
+  let theNew = Object.assign({}, obj);
+  delete theNew[key];
+  return theNew;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
